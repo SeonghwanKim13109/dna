fa1d7458c9bf97aa8283dcedf86146e0d50e952e
seonghwan <reasonby13@naver.com>
2018-09-02 19:38:43 -0700

    factorial using recursion

--- DnA01/src/Recursive.java
+++ DnA01/src/Recursive.java
@@ -4,6 +4,7 @@ public class Recursive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(func(10));
+		System.out.println(factorial(0));
 	}
 
 	public static int func(int i) {
@@ -17,6 +18,21 @@ public class Recursive {
 			else
 				System.out.print(i + " + " );
 			return i + func(i - 1);
+		}
+	}
+	
+	public static int factorial(int n) {
+		if(n == 0) {
+			System.out.print("0! = ");
+			return 1;
+		}
+		else if(n ==1) {
+			System.out.print("1 = ");
+			return 1;
+		}
+		else {
+			System.out.print(n + " * ");
+			return n*factorial(n-1);
 		}
 	}
 }
