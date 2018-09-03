33d4796eb1ec8136f7bb975f1d9c0f6f55781f3e
seonghwan <reasonby13@naver.com>
2018-09-03 01:36:49 -0700

    print character using recursion

--- DnA02/src/Recursive2.java
+++ DnA02/src/Recursive2.java
@@ -4,6 +4,7 @@ public class Recursive2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(stringLenth("i love you"));
+		printString("i love you");
 	}
 	
 	public static int stringLenth(String str) {
@@ -11,5 +12,16 @@ public class Recursive2 {
 			return 0;
 		else
 			return 1+stringLenth(str.substring(1));
+	}
+	
+	public static void printString(String str) {
+		if(stringLenth(str) == 0) {
+			System.out.println();
+			return;
+		}
+		else {
+			System.out.print(str.charAt(0));
+			printString(str.substring(1));
+		}
 	}
 }
