79700cb761b50d0b3e3e214bbdecf1962817f4e3
seonghwan <reasonby13@naver.com>
2018-09-02 19:34:31 -0700

    display current number

--- DnA01/src/Recursive.java
+++ DnA01/src/Recursive.java
@@ -3,13 +3,20 @@ public class Recursive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(func(4));
+		System.out.println(func(10));
 	}
+
 	public static int func(int i) {
 //		base case : condition to get out from recursion
-		if(i == 0 )
+		if (i == 0)
 			return 0;
 //		recursive case
-		return i + func(i-1);
+		else {
+			if(i == 1)
+				System.out.print("1 = ");
+			else
+				System.out.print(i + " + " );
+			return i + func(i - 1);
+		}
 	}
 }
