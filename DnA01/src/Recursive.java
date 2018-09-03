2841dad741fe763ba9995a5b2319c8b4efb0f65d
seonghwan <reasonby13@naver.com>
2018-09-02 19:31:58 -0700

    implement sum using recursive

--- DnA01/src/Recursive.java
+++ DnA01/src/Recursive.java
@@ -3,12 +3,13 @@ public class Recursive {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		func(4);
+		System.out.println(func(4));
 	}
-	public static void func(int i) {
-		if(i<=0)
-			return;
-		System.out.println("Hello World");
-		func(i-1);
+	public static int func(int i) {
+//		base case : condition to get out from recursion
+		if(i == 0 )
+			return 0;
+//		recursive case
+		return i + func(i-1);
 	}
 }
